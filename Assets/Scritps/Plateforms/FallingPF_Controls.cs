eac324f4c3f009422944470b8a6b4dd7ed097ac2
FishyTree <53624883+camcartier@users.noreply.github.com>
2026-06-17 14:40:34 +0200

    finished falling pf

--- Assets/Scritps/Plateforms/FallingPF_Controls.cs
+++ Assets/Scritps/Plateforms/FallingPF_Controls.cs
@@ -9,12 +9,16 @@ public class FallingPF_Controls : MonoBehaviour
     private Collider2D coll2D;
     private Rigidbody2D rigidbody2;
 
+    private Vector2 initPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         coll2D = GetComponentInChildren<Collider2D>();
         rigidbody2 = GetComponent<Rigidbody2D>();
+
+        initPosition = transform.position;
     }
 
 
@@ -37,5 +41,12 @@ public class FallingPF_Controls : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         coll2D.enabled = false;
 
+        yield return new WaitForSeconds(3f);
+        spriteRenderer.color = Color.white;
+        gameObject.transform.position = initPosition;
+        rigidbody2.constraints = RigidbodyConstraints2D.FreezeAll;
+        rigidbody2.gravityScale = 1f;
+        coll2D.enabled = true;
+        StopAllCoroutines();
     }
 }
