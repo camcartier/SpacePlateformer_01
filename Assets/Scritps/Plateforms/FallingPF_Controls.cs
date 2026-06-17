cd5f0ffaf071e462affa937ddc9b509205b1d56b
FishyTree <53624883+camcartier@users.noreply.github.com>
2026-06-17 14:26:42 +0200

    added falling plateforms

---
+++ Assets/Scritps/Plateforms/FallingPF_Controls.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class FallingPF_Controls : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Collider2D coll2D;
+    private Rigidbody2D rigidbody2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        coll2D = GetComponentInChildren<Collider2D>();
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PlateformFalls());
+        }
+    }
+
+    public IEnumerator PlateformFalls()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(1f);
+
+        rigidbody2.constraints = RigidbodyConstraints2D.None;
+        rigidbody2.gravityScale = 8f;
+        
+        yield return new WaitForSeconds(0.5f);
+        coll2D.enabled = false;
+
+    }
+}
