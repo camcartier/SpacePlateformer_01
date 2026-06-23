0ade72154dcc8ce9604c20cb8517258ee5c19179
FishyTree <53624883+camcartier@users.noreply.github.com>
2026-06-23 19:16:00 +0200

    random sprite material and now in urp we'll see what to do with that

--- Assets/Scritps/Asteroids/AsteroidControls.cs
+++ Assets/Scritps/Asteroids/AsteroidControls.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class AsteroidControls : MonoBehaviour
 {
+    [SerializeField] SpriteRenderer spriteRenderer;
+    private int chosenSpriteNumber;
+
+    public SpriteRenderer[] arrayOfSprites = new SpriteRenderer[5];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+
+        chosenSpriteNumber = Random.Range(0, arrayOfSprites.Length);
+
+        for (int i = 0; i < arrayOfSprites.Length; i++)
+        {
+            if (i == chosenSpriteNumber)
+            {
+                arrayOfSprites[i].enabled = true;
+            }
+            else
+            {
+                arrayOfSprites[i].enabled = false;
+            }
+        }
+
+        spriteRenderer.sprite = arrayOfSprites[chosenSpriteNumber].sprite;
     }
 
     // Update is called once per frame
