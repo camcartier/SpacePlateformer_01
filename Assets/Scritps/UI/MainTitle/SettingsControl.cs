e2dccc5f93734138998760904aa9b9d20c4f4251
FishyTree <53624883+camcartier@users.noreply.github.com>
2026-06-28 20:27:23 +0200

    now has mixer working and can go to windowed

--- Assets/Scritps/UI/MainTitle/SettingsControl.cs
+++ Assets/Scritps/UI/MainTitle/SettingsControl.cs
@@ -6,8 +6,23 @@ using UnityEngine.Audio;
 public class SettingsControl : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public void SetVolume(float volume)
+    public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetMusicVolume (float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void SetSfxsVolume(float volume)
+    {
+        audioMixer.SetFloat("SfxsVolume", volume);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        if (isFullScreen) { Screen.fullScreenMode = FullScreenMode.FullScreenWindow; }
+        else { Screen.fullScreenMode = FullScreenMode.Windowed; }
     }
 }
